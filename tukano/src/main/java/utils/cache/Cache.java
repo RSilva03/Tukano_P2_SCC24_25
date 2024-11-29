42a843aa6d1b90dbcb701fef5a1c1dc21a3a3238
Apocalipse15 <a.salselas@campus.fct.unl.pt>
2024-11-29 21:19:58 +0000

    Refactor the files

---
+++ tukano/src/main/java/utils/cache/Cache.java
@@ -0,0 +1,49 @@
+package utils.cache;
+
+public class Cache {
+
+    public static String get(String key) {
+        var jedis = RedisCache.getCachePool().getResource();
+        if(jedis.exists(key)){
+            return jedis.get(key);
+        } else{
+            return null;
+        }
+    }
+
+    public static void put(String key, String value) {
+        try{
+            var jedis = RedisCache.getCachePool().getResource();
+
+            jedis.set( key, value );
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void replace(String key, String value){
+        try {
+            var jedis = RedisCache.getCachePool().getResource();
+            if(jedis.exists(key)){
+                jedis.del(key);
+                jedis.set( key, value );
+            } else{
+                jedis.set( key, value );
+            }
+        } catch (Exception e){
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void delete(String key){
+        try {
+            var jedis = RedisCache.getCachePool().getResource();
+            if(jedis.exists(key)) {
+                jedis.del(key);
+            }
+        } catch (Exception e){
+            throw new RuntimeException(e);
+        }
+    }
+
+}
